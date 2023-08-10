e5fcf74a2c5caa59d938ec020d60f54973ab89cb
francescabasini <francesca.basini@gmail.com>
2023-08-10 16:06:32 +0100

    your-commit

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#setting up GitHub username and token
+USERNAME = "francescabasini"
+TOKEN = "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"
 
 git add .
 git commit -m "your-commit"
 git push origin master
+
+read -sp "Username for 'https://github.com':" $USERNAME
 
