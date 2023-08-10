b71c8cc5039b2bb6947732432efb25178ece4e57
francescabasini <francesca.basini@gmail.com>
2023-08-10 16:17:40 +0100

    your-commit

--- git_push.sh
+++ git_push.sh
@@ -2,14 +2,16 @@
 
 #setting up GitHub username and token
 USERNAME="francescabasini"
-TOKEN="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"
+PASSWORD="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"
 
 git add .
 git commit -m "your-commit"
 git push origin master
 
+# Configure Git to use the provided username and password for the next push
+git config credential.helper "!f() { echo username=${USERNAME}; echo password=${PASSWORD}; }; f"
 
-echo "Username for 'https://github.com': $USERNAME"
+echo "Pushed changes to GitHub using provided credentials."
 
 #display 
 
